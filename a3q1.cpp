b4a3e1ec103c8b706406932941c24c086562e8c7
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 19:43:03 +0530

    Create a3q1.cpp

---
+++ a3q1.cpp
@@ -0,0 +1,18 @@
+# cs-assignment-3
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   cout<<"enter any two numbers \n"<<endl;
+   cin>>a>>b;
+   if(a<b)
+   {
+     cout<<"the maximum number of the two numbers is "<<b;
+   }
+   else
+   {   
+     cout<<"the maximum number of the two numbers is "<<a;
+   }
+   return 0;
+}
