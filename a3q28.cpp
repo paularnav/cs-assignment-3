45efe58a71952764803361362dbc735a939916be
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:41:10 +0530

    Create a3q28.cpp

---
+++ a3q28.cpp
@@ -0,0 +1,19 @@
+# cs-assignment-3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int n,s=0,i;
+	cout<<"enter n natural number\n";
+	cin>>n;
+	i=2;
+	while(i<=n)
+	{
+		s=s+i;
+		i=i+2;
+	}
+	cout<<"the sum of all even numbers is ="<<s;
+	return 0;
+}
