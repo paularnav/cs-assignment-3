02a782bea7391dc598f8719f7f4d877dd2a89e87
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:43:02 +0530

    Create a3q32.cpp

---
+++ a3q32.cpp
@@ -0,0 +1,17 @@
+# cs-assignment-3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,l,f;
+	cout<<"enter a number\n";
+	cin>>n;
+	l=n%10;
+	while(n>0)
+	{
+		f=n%10;
+		n=n/10;
+	}
+	cout<<"the first digit in a given number is "<<f<<"\n"<<"the last digit is "<<l;
+	return 0;
+}
