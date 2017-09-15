50b6e97b712ccf8add5c587d12fa65652576a6b3
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:38:07 +0530

    Create a3q23.cpp

---
+++ a3q23.cpp
@@ -0,0 +1,16 @@
+# cs-assignment-3
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,n;
+ cout<<"Enter a number : ";
+ cin>>n;
+ a=1;
+ while(a<=n)
+ {
+ cout<<n<<"	";
+ --n;
+ }
+ return 0;
+}
