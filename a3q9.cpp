6322edd2879943f5f196e02b85f0e5f581eae086
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 19:53:34 +0530

    Create a3q9.cpp

---
+++ a3q9.cpp
@@ -0,0 +1,23 @@
+# cs-assignment-3
+#include<iostream>
+using namespace std;
+int main()
+{
+  char c;
+  cout<<"enter a character\n";
+  cin>>c;
+
+  if(c>='0'&&c<='9')
+  {
+     cout<<c<<" is a digit";
+  }
+  else if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+  {
+     cout<<c<<" is an alphabet"<<endl;
+  }
+  else
+  {
+     cout<<c<<" is a special character"<<endl;
+  }
+  return 0;
+}
