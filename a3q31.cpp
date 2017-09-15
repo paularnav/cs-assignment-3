eb0e3bb9098a728363a121ac8085e3f84bdeef42
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:42:36 +0530

    Create a3q31.cpp

---
+++ a3q31.cpp
@@ -0,0 +1,16 @@
+# cs-assignment-3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i;
+	cout<<"enter a number\n";
+	cin>>n;
+	while(n>0)
+	{
+		n=n/10;
+		i++;
+	}
+	cout<<"the digits in a given number is "<<i;
+	return 0;
+}
