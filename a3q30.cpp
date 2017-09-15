947142b0817726497206d37fe117f4d32f62ea39
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:42:10 +0530

    Create a3q30.cpp

---
+++ a3q30.cpp
@@ -0,0 +1,17 @@
+# cs-assignment-3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,m;
+	cout<<"enter any number\n";
+	cin>>n;
+	i=1;
+	while(i<=10)
+	{
+	  m=n*i;
+	  cout<<n<<'*'<<i<<'='<<m<<"\n";
+	  i++;
+	}
+	return 0;
+}
