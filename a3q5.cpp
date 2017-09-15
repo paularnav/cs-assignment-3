31b1e6281ba6ea281fea9ab4c87766610d4eb8d9
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 19:48:30 +0530

    Create a3q5.cpp

---
+++ a3q5.cpp
@@ -0,0 +1,18 @@
+# cs-assignment-3
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"Enter a number\n"<<endl;
+  cin>>a;
+  if(a%2==0)
+  {
+    cout<<a<<" Is an even number"<<endl;
+  }
+  else
+  {
+    cout<<a<<" Is an odd number"<<endl;
+  }
+  return 0;
+}
