acc6cb60f282c34b3f5dbf3cb90151c03fc5e004
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:56:06 +0530

    Create a3q42.cpp

---
+++ a3q42.cpp
@@ -0,0 +1,17 @@
+# cs-assignment-3
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int n,i,p=1,e;
+	cout<<"enter a number and exponent\n";
+	cin>>n>>e;
+	for(i=1;i<=e;i++)
+	{
+		p=p*n;
+	}
+	cout<<"the power a number is " <<p;
+	
+	return 0;
+}
