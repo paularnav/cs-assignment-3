ba2b877d46ab4615978af9a0c8c7ecb82aac53df
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:54:11 +0530

    Create a3q41.cpp

---
+++ a3q41.cpp
@@ -0,0 +1,13 @@
+# cs-assignment-3
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+            char c;int a;
+            cout<<"enter a character\n";
+            cin>>c;
+            a=int(c);
+            cout<<"the ascii code of the character "<<a; 
+	return 0;
+}
