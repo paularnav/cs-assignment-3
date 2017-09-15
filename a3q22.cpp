e75fce014d416dfa758bed11dac5a3d63b8e5fa0
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 20:37:14 +0530

    Create a3q22.cpp

---
+++ a3q22.cpp
@@ -0,0 +1,15 @@
+# cs-assignment-3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i=1;
+	cout<<"enter n natural number\n";
+	cin>>n;
+	while(i<=n)
+	{
+		cout<<i<<"\n";
+		i++;
+	}
+	return 0;
+}
