da107a495046dbab352b7352ab0d622cc613130a
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-15 19:51:21 +0530

    Create a3q7.cpp

---
+++ a3q7.cpp
@@ -0,0 +1,18 @@
+# cs-assignment-3
+#include<iostream>
+using namespace std;
+int main()
+{
+   char c;
+   cout<<"enter a character\n";
+   cin>>c;
+   if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+   {
+     cout<<c<<" is an alaphabet";
+   }
+   else
+   {
+     cout<<c<<" is not an alphabet";
+   }
+   return 0;
+}
